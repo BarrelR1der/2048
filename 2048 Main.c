e310874150cd74293727b334673202d18609c6da
BarrelR1der <72632779+BarrelR1der@users.noreply.github.com>
2021-11-04 15:19:25 -0400

    Update 2048 Main.c

--- 2048 Main.c
+++ 2048 Main.c
@@ -14,6 +14,9 @@
 */
 
 #include "stm32l053xx.h"
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 
 
@@ -31,7 +34,70 @@ extern void down(void);
 extern void left(void);      
 extern void right(void);
 
+struct Queue {
+    int front, rear, size;
+    unsigned capacity;
+    int* array;
+};
 
+struct Queue* createQueue(unsigned capacity)
+{
+    struct Queue* queue = (struct Queue*)malloc(
+        sizeof(struct Queue));
+    queue->capacity = capacity;
+    queue->front = queue->size = 0;
+ 
+    // This is important, see the enqueue
+    queue->rear = capacity - 1;
+    queue->array = (int*)malloc(
+        queue->capacity * sizeof(int));
+    return queue;
+}
+
+int isFull(struct Queue* queue)
+{
+    return (queue->size == queue->capacity);
+}
+
+int isEmpty(struct Queue* queue)
+{
+    return (queue->size == 0);
+}
+ 
+void enqueue(struct Queue* queue, int item)
+{
+    if (isFull(queue))
+        return;
+    queue->rear = (queue->rear + 1)
+                  % queue->capacity;
+    queue->array[queue->rear] = item;
+    queue->size = queue->size + 1;
+}
+
+int dequeue(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    int item = queue->array[queue->front];
+    queue->front = (queue->front + 1)
+                   % queue->capacity;
+    queue->size = queue->size - 1;
+    return item;
+}
+
+int front(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->front];
+}
+
+int rear(struct Queue* queue)
+{
+    if (isEmpty(queue))
+        return INT_MIN;
+    return queue->array[queue->rear];
+}
 
 
 void init_display(void)
@@ -185,6 +251,9 @@ void turn_off_all(void)
 
 int main(void)
 {
+	//setting up the queues
+	struct Queue* ButtonQueue = createQueue(1);
+   	struct Queue* DisplayQueue = createQueue(16);
 	// gpio
 	init_gpio();
 	// spi
