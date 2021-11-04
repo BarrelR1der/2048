b06a8c73de187b6748c60191b67ab70e3a660ca4
ekdickerson17 <79267433+ekdickerson17@users.noreply.github.com>
2021-11-04 14:54:17 -0400

    Create btn.h

---
+++ btn.h
@@ -0,0 +1,19 @@
+#include "stm32l053xx.h"
+#include <stdint.h>
+enum btn_status{UP, DOWN, MAYBE_PRESSED, MAYBE_RELEASED};
+
+enum btn_level {BTN_ACTIVE, BTN_INACTIVE};
+
+enum btn_edge {ACTIVE, INACTIVE, ACTIVATING_EDGE, DEACTIVATING_EDGE};
+
+struct btn_struct{uint32_t mask_for_btn; 
+                  volatile uint32_t *pin_ptr; 
+                  enum btn_status state;};
+
+void init_btn(uint32_t mask_for_btn, 
+              volatile uint32_t *pin_ptr, 
+              struct btn_struct *pb);
+
+enum btn_level read_btn(struct btn_struct *pb);
+ 
+enum btn_edge update_btn(struct btn_struct *pb);
