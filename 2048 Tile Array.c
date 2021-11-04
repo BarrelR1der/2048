b9051d4b396e8be2fea39be5d968d0430a6523d2
cmthatcher21 <91984380+cmthatcher21@users.noreply.github.com>
2021-11-04 15:23:55 -0400

    Add files via upload

--- 2048 Tile Array.c
+++ 2048 Tile Array.c
@@ -2,29 +2,102 @@
 #include "stm32l053xx.h"
 
 // Function List
-void Move_Left(void);
-void Move_Right(void);
-void Move_Down(void);
-void Move_Up(void);
+void Moving(uint32_t direction);
 
-int TileArray[16]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+int static TA[16]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-void Move_Left(void)
+
+void Moving(uint32_t direction)
 {
-	
-}
-
-void Move_Right(void)
-{
-
-}
-
-void Move_Down(void)
-{
-	
-}
-
-void Move_Up(void)
-{
+int move = Queue[0];
+switch (move) {
+            case 0: //UP
+                if ( TA[12]==TA[8] && TA[4]==TA[0])
+								{
+									TA[8]= TA[12]+TA[8];
+									TA[0]= TA[4]+TA[0];
+								}
+								else if (TA[12]==TA[8])
+								{
+									TA[8]=TA[12]+TA[8];
+								}
+								else if (TA[8]==TA[4])
+								{
+									TA[4]=TA[8]+TA[4];
+								}
+								else if (TA[4]==TA[0])
+								{
+									TA[0]=TA[4]+TA[0];
+								}
+								if (TA[13]==TA[9] && TA[5]==TA[1])
+								{
+									TA[9]=TA[13]+TA[9];
+									TA[1]=TA[5]+TA[1];
+								}
+								else if (TA[13]==TA[9])
+								{
+									TA[9]=TA[13]+TA[9];
+								}
+								else if (TA[9]==TA[5])
+								{
+									TA[5]=TA[9]+TA[5];
+								}
+								else if (TA[5]==TA[1])
+								{
+									TA[1]=TA[5]+TA[1];
+								}
+								if (TA[14]==TA[10] && TA[6]==TA[2])
+								{
+									TA[10]=TA[14]+TA[10];
+									TA[2]=TA[6]+TA[2];
+								}
+								else if (TA[14]==TA[10])
+								{
+									TA[10]=TA[14]+TA[10];
+								}
+								else if (TA[10]==TA[6])
+								{
+									TA[6]=TA[10]+TA[6];
+								}
+								else if (TA[6]==TA[2])
+								{
+									TA[2]=TA[6]+TA[2];
+								}
+								if (TA[15]==TA[11] && TA[7]==TA[3])
+								{
+									TA[11]=TA[15]+TA[11];
+									TA[3]=TA[7]+TA[3];
+								}
+								else if (TA[15]==TA[11])
+								{
+									TA[11]=TA[15]+TA[11];
+								}
+								else if (TA[11]==TA[7])
+								{
+									TA[7]=TA[11]+TA[7];
+								}
+								else if (TA[7]==TA[3])
+								{
+									TA[3]=TA[7]+TA[3];
+								}
+							
+								
+									
+                break;
+            case 1: // Right
+                
+                break;
+            case 2: // Left
+                
+                break;
+						case 3: // Down
+							
+							break;
+						
+            default:
+                printf("Out of range");
+                break;
+        }
+        return 0;
 
 }
