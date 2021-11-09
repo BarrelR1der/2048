d44b053de2f5765803f82d69a07b25ea17b26eca
ekdickerson17 <79267433+ekdickerson17@users.noreply.github.com>
2021-11-09 15:49:13 -0500

    Update knob_main.c

--- knob_main.c
+++ knob_main.c
@@ -2,37 +2,7 @@
 #include "stdint.h"
 #include "btn.h"
 
-
-void gpio_init(void);
 int main(void);
-void gpio_init()
-{
-    RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
-	
-    GPIOB->MODER &= ~GPIO_MODER_MODE8_Msk;
-    GPIOB->MODER |= 0 << GPIO_MODER_MODE8_Pos; // PB8 Digital INPUT
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD8_Msk;
-    GPIOB->PUPDR |= 0x01 << GPIO_PUPDR_PUPD8_Pos; // Pull-up engaged
-    
-	  
-    GPIOB->MODER &= ~GPIO_MODER_MODE9_Msk;
-    GPIOB->MODER |= 0 << GPIO_MODER_MODE9_Pos; // PB9 Digital INPUT
-    GPIOB->PUPDR &= ~GPIO_PUPDR_PUPD9_Msk;
-    GPIOB->PUPDR |= 0x01 << GPIO_PUPDR_PUPD9_Pos; // Pull-up engaged
-	
-	  GPIOA->MODER &= ~GPIO_MODER_MODE2_Msk;
-    GPIOA->MODER |= 0 << GPIO_MODER_MODE2_Pos; // PA2 Digital INPUT
-    GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD2_Msk;
-    GPIOA->PUPDR |= 0x01 << GPIO_PUPDR_PUPD2_Pos; // Pull-up engaged
-	
-		GPIOA->MODER &= ~GPIO_MODER_MODE3_Msk;
-    GPIOA->MODER |= 0 << GPIO_MODER_MODE3_Pos; // PA3 Digital INPUT
-    GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD3_Msk;
-    GPIOA->PUPDR |= 0x01 << GPIO_PUPDR_PUPD3_Pos; // Pull-up engaged
-	
-	   
-}
-
 
 int main( )
 {
@@ -64,10 +34,10 @@ int main( )
 		init_btn(1 << 3, &(GPIOA->IDR), &btn_d); // Init state machine for PA3			
     
     while(1){
-        ea = update_btn(&btn_a);
-				eb = update_btn(&btn_b);
-				ec = update_btn(&btn_c);
-				ed = update_btn(&btn_d);
+        	ea = update_btn(&btn_a);
+		eb = update_btn(&btn_b);
+		ec = update_btn(&btn_c);
+		ed = update_btn(&btn_d);
 	    
 		//Send a 1 to the queue for right movement
 		if (ea == ACTIVATING_EDGE && eb == INACTIVE){
